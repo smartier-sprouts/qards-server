333c16aa6f18d4c0e9cf33dbc621d40af3e2056c
Henri Pietila <henrismail@gmail.com>
2017-07-14 14:55:43 -0700

    socket io logic for singaling webrtc

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,3 @@
 module.exports.api = require('./api');
-module.exports.auth = require('./auth');
-module.exports.profiles = require('./profiles');
+//module.exports.auth = require('./auth');
+//module.exports.profiles = require('./profiles');
