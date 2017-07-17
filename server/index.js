01e711709bafd68826e694b35cdb089d154a80aa
Jon Anderson <janderson215@gmail.com>
2017-07-16 23:52:11 -0700

    (fix) references to PORT pt 2

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 'use strict';
 const app = require('./app');
 // const db = require('../db');
+const PORT = process.env.PORT || 3000;
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
