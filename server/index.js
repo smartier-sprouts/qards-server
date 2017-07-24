f84c7363aa6bfa6d69b5879460e8c53b5a590111
Henri Pietila <henrismail@gmail.com>
2017-07-23 22:33:15 -0700

    fixed case error

--- server/index.js
+++ server/index.js
@@ -8,18 +8,7 @@ const server =  app.listen(PORT, () => {
 });
 
 // var server = require('http').createServer(app);
-const io = require('socket.io').listen(server);
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
 
-  socket.on('create', function(room) {
-    console.log('new player joins');
-    socket.join(room);
-    io.to(room, 'a new user has joined the room');
-  });
-
-});
 //setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
-module.exports.server = server;
+module.exports = server;
