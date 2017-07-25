de1a0496d422859e0c9df69bd7f2f918f9ff7b7d
Henri Pietila <henrismail@gmail.com>
2017-07-24 20:12:31 -0700

    socket test3

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -1,20 +1,25 @@
 const server = require('../index.js').server;
 
-const io = require('socket.io').listen(server);
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
 
-  socket.on('create', function(room) {
-    console.log('new player joins');
-    socket.join(room);
-    io.to(room, 'a new user has joined the room');
-  });
-
-});
 
 const emitPlayerNumber = (gameId) => {
-  io.emit('test', { hello: 'world' });
+  console.log('in emitPlayerNumber');
+
+  const io = require('socket.io').listen(server);
+  io.on('connection', (socket) => {
+    console.log('Client connected');
+    socket.on('disconnect', () => console.log('Client disconnected'));
+
+    socket.on('create', function(room) {
+      console.log('new player joins');
+      socket.join(room);
+      io.to(room, 'a new user has joined the room');
+    });
+    io.emit('test', { hello: 'world' });
+  });
+
+
+
 };
 
 
