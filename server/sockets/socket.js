73617b6e5d0daef2da34a6d47d54eeafef02e0d1
Henri Pietila <henrismail@gmail.com>
2017-07-24 20:31:13 -0700

    make work

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -10,13 +10,18 @@ const emitPlayerNumber = (gameId) => {
     console.log('Client connected');
     socket.on('disconnect', () => console.log('Client disconnected'));
 
+    socket.emit('test', { hello: 'world' });
+
+
     socket.on('create', function(room) {
       console.log('new player joins');
       socket.join(room);
       io.to(room, 'a new user has joined the room');
     });
-    io.emit('test', { hello: 'world' });
+    socket.emit('test', { hello: 'world' });
   });
+  socket.emit('test', { hello: 'world!!!' });
+
 
 
 
