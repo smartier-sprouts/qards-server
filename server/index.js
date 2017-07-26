a6d23d1cd33a1839f1b9589383f54ac40bf5a5e9
Henri Pietila <henrismail@gmail.com>
2017-07-26 10:53:09 -0700

    modified server

--- server/index.js
+++ server/index.js
@@ -10,26 +10,13 @@ const server =  app.listen(PORT, () => {
 const io = require('socket.io').listen(server);
 const serverEmitter = new EventEmitter();
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
+
+io.sockets.on('connection', (socket) => {
+  console.log('client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 
-  socket.emit('test', { hello: 'world' });
-
-
-  socket.on('create', function(room) {
-    console.log('new player joins');
-    socket.join(room);
-    io.to(room, 'a new user has joined the room');
-  });
-
-  serverEmitter.on('playerJoin', function (data) {
-    socket.emit(data);
-  });
-
-  setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
-
 });
-//setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
 module.exports = server;
