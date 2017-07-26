a8a6f48a889c075aeb6c3e1954937f17d3b3e5d3
Henri Pietila <henrismail@gmail.com>
2017-07-26 11:35:51 -0700

    server emits in

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 'use strict';
+const EventEmitter = require('events');
 const app = require('./app');
+
 // const db = require('../db');
 const PORT = process.env.PORT || 3000;
 
@@ -8,15 +10,18 @@ const server =  app.listen(PORT, () => {
 });
 
 const io = require('socket.io').listen(server);
-//const serverEmitter = new EventEmitter();
+const serverEmitter = new EventEmitter();
 
 
 io.sockets.on('connection', (socket) => {
   console.log('client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
-
+  serverEmitter.on('playerJoin', (data) => {
+      io.emit('playerJoin', {players : 5});
+  });
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+setInterval(() =>( serverEmitter.emit('playerJoin','yo'), 2000 ))
+//setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
 module.exports = server;
